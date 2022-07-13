87dd744ce6c7da85e2f6b00c6c7a429d48f0d4b5
Giancarlo Musetti <giancarlomusetti@Giancarlos-MBP.lan>
2022-07-13 15:45:21 -0700

    making it work!

--- tinder_bot_July 3.py
+++ tinder_bot_July 3.py
@@ -3,7 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver = webdriver.Chrome(ChromeDriverManager().install())
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
@@ -21,67 +21,73 @@ class TinderBot():
 
         time.sleep(2)
 
-        login_btn = self.driver.find_element(By.XPATH, '//*[@id="q-996647900"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')
+        login_btn = self.driver.find_element(By.XPATH, '//*[@id="c464932099"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')  
         login_btn.click()
         
-        window_before = driver.window_handles[0]
+        window_before = self.driver.window_handles[0]
         
          # click 'I agree' and facebook button
-        agree_btn = self.driver.find_element(By.XPATH, '//*[@id="q-996647900"]/div/div[2]/div/div/div[1]/div[1]/button')
+        agree_btn = self.driver.find_element(By.XPATH, '//*[@id="c464932099"]/div/div[2]/div/div/div[1]/div[1]/button')
         agree_btn.click()
         
         time.sleep(5)
 
-        fb_btn = self.driver.find_element(By.XPATH, '//*[@id="q1569938320"]/div/div/div[1]/div/div/div[3]/span/div[2]/button')
+        fb_btn = self.driver.find_element(By.XPATH, '//*[@id="c-1263448977"]/div/div/div[1]/div/div/div[3]/span/div[2]/button')
         fb_btn.click() 
 
-        for handle in driver.window_handles:
+        time.sleep(2)
+
+        for handle in self.driver.window_handles:
             if handle != window_before:
                 login_page = handle
 
-        driver.switch_to.window(login_page)
+        time.sleep(7)
 
+        self.driver.switch_to.window(login_page)
+
+        time.sleep(3)
         # switch to window 
         # window_after = driver.window_handles[1]
         # driver.switch_to.window(window_after)
 
-        
         email_in = self.driver.find_element(By.XPATH, '//*[@id="email"]')   
         email_in.click()
         email_in.send_keys('ENTER EMAIL')
 
         pw_in = self.driver.find_element(By.XPATH, '//*[@id="pass"]')
-        pw_in.send_keys('ENTER PASSWORD')
+        pw_in.send_keys('ENTER PASS')
 
-        login_btn = self.driver.find_element(By.XPATH, '//*[@id="u_0_0"]')
+        login_btn = self.driver.find_element(By.XPATH, '//*[@id="loginbutton"]')
         login_btn.click()
+
+        time.sleep(3)
 
         self.driver.switch_to.window(window_before)
 
-        popup_1 = self.driver.find_element(By.XPATH, '//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_1 = self.driver.find_element(By.XPATH, '//*[@id="c-1263448977"]/div/div/div/div/div[3]/button[1]')
         popup_1.click()
 
-        popup_2 = self.driver.find_element(By.XPATH, '//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_2 = self.driver.find_element(By.XPATH, '//*[@id="c-1263448977"]/div/div/div/div/div[3]/button[1]')
         popup_2.click()
 
     def like(self):
-        like_btn = self.driver.find_element(By.XPATH, '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]')
-        like_btn.click()
+        like_btn = self.driver.find_element(By.XPATH, '//*[@id="c464932099"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[4]/button') 
+        like_btn.click() 
 
     def dislike(self):
-        dislike_btn = self.driver.find_element(By.XPATH, '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[1]')
+        dislike_btn = self.driver.find_element(By.XPATH, '//*[@id="c464932099"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[2]/button')
         dislike_btn.click()
 
     def auto_swipe(self):
         while True:
-            sleep(0.5)
-            try:
-                self.like()
-            except Exception:
-                try:
-                    self.close_popup()
-                except Exception:
-                    self.close_match()
+            time.sleep(0.5)
+            # try:
+            self.like()
+            # except Exception:
+            #     try:
+            #         self.close_popup()
+            #     except Exception:
+            #         self.close_match()
 
     def close_popup(self):
         popup_3 = self.driver.find_element(By.XPATH, '//*[@id="modal-manager"]/div/div/div[2]/button[2]')
@@ -93,3 +99,4 @@ class TinderBot():
 
 bot = TinderBot()
 bot.login()
+bot.auto_swipe()
